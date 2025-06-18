385a207f41e6cbb6490de585ccfc4293208865a9
Nebiyu Daniel <nebiyu.daniel@stu.ocsb.ca>
2025-06-18 15:42:28 +0000

    gh

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+"use strict";
+function Age() {
+    let ans= "";
+    let WatchMove = parseInt(document.getElementById("WatchMove").value);
+   
+    if (WatchMove >= 17) {
+        ans = "You can watch PG18 Moves"
+    }
+    else if (WatchMove >= 13) {
+        ans = "You are only allowed to watch PG13";
+    }
+    else if (WatchMove <= 12) {
+        ans = "You are only allowed to watch PG";
+    }
+    else {
+        ans = "Enter a Valid Age";
+    }
+    document.getElementById("ans").innerHTML = ans ;
+}
